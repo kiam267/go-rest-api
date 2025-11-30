febdf6d82047d9b02a97fbf72c436f213cf8e052
Abid Hasan Kiam <kiamhasan267@gmail.com>
2025-11-30 15:56:11 +0600

    refactor: enhance server configuration and improve shutdown handling

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/kiam267/student-api/internal/config"
+)
 
 func main() {
-	fmt.Printf("GO API - Larning ");
+	//  Load Config
+	cfg := config.MustLoad()
+
+	// database setup 
+	// setup router
+	router:= http.NewServeMux()
+
+	router.HandleFunc("GET /", func (w http.ResponseWriter, r *http.Request)  {
+		w.Write([]byte("Welcome to student api "))
+	})
+	// setup server
+
+	server := http.Server{
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+	slog.Info("Server Started", slog.String("address",cfg.Addr,))
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+
+  go func ()  {
+		err:= server.ListenAndServe()
+
+		if err != nil {
+			log.Fatal("Failed to start server")
+		}
+	}()
+
+	<-done
+  slog.Info("Shutting down the server")
+	
+	ctx, _ := context.WithTimeout(context.Background(), 5 * time.Second)
+ 
+ if err:= server.Shutdown(ctx);err != nil {
+	slog.Error("Failed to Shutdown server", slog.String("error", err.Error()))
+ }
+ slog.Info("server Shutdown successfully");
+
 }
